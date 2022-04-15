ffe079b92818699bb521cfe36e83170b368dc73e
ortori <ortori@gmail.com>
2022-04-15 13:06:40 +0300

    .

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const signUpLink = document.querySelector(".sign-up-link");
 const logInLink = document.querySelector(".log-in-link");
 const logInScreen = document.querySelector(".login-container");
 const signUpScreen = document.querySelector(".sign-up-container");
+const mainScreen = document.querySelector(".main-screen-container");
 const signUpBtnSubmit = document.querySelector(".sign-up-btn-submit");
 const logInBtn = document.querySelector(".log-in-btn");
 const signUpForm = document.forms.signUpForm;
-const users = localStorage.getItem("data") ? localStorage.getItem("data") : [];
+const mainHeading = document.querySelector(".main-heading");
+const users = localStorage.getItem("data")
+  ? JSON.parse(localStorage.getItem("data"))
+  : [];
+let currentUserIndex;
 // Toggeling between the login and signup screens
 function toggleSignUpLogInScreens() {
   logInScreen.classList.toggle("d-none");
@@ -57,7 +62,55 @@ signUpBtnSubmit.addEventListener("click", function (e) {
     toggleSignUpLogInScreens();
   }
 });
+// Function to validate login details
+function validateLogin(array, username, password) {
+  let userIndex;
+  array.forEach((item, i) => {
+    if (item.username === username) {
+      userIndex = i;
+    }
+  });
+  if (userIndex >= 0) {
+    if (array[userIndex].password === password) {
+      console.log("Logged in succesfully");
+      currentUserIndex = userIndex;
+      return true;
+    } else {
+      alert("Wrong password! Please try again!");
+    }
+  } else {
+    alert(
+      "Username does not exist in the database! Please try a different username or sign up!"
+    );
+  }
+}
+// Function that hides the login screen and display the main screen
+function displayMainScreen() {
+  logInScreen.classList.add("d-none");
+  signUpScreen.classList.add("d-none");
+  mainScreen.classList.toggle("d-none");
+}
+// Function that displays the main heading according to user's name
+function mainHeadingContent() {
+  const hour = new Date().getHours();
+  let greeting;
+  if (hour < 12 && hour > 6) greeting = "morning";
+  else if (hour > 12 && hour < 18) greeting = "afternoon";
+  else if (hour > 18 && hour < 24) greeting = "evening";
+  else greeting = "night";
+  mainHeading.textContent = `Hello ${users[currentUserIndex].name}, have a good ${greeting}`;
+}
+// Actions that will happend after the user click on the "log in" button
 logInBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("logged in succesfully");
+  if (
+    validateLogin(
+      users,
+      document.forms.logInForm.username.value,
+      document.forms.logInForm.password.value
+    )
+  ) {
+    displayMainScreen();
+    mainHeadingContent();
+  }
 });
